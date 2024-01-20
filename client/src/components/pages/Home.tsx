586e86eb78f71a17d6e57ff3833ce8ddbb60471f
Christy Li <christyli@dhcp-10-31-158-19.dyn.MIT.EDU>
2024-01-19 21:32:04 -0500

    start of homepage

---
+++ client/src/components/pages/Home.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  googleLogout,
+  CredentialResponse,
+} from "@react-oauth/google";
+
+import "./Home.css";
+
+//TODO(weblab student): REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "924135144483-a4h7ghgqcej244vnv4312rdkd4lovc95.apps.googleusercontent.com";
+
+type Props = {
+  userId?: string;
+  handleLogin: (credentialResponse: CredentialResponse) => void;
+  handleLogout: () => void;
+};
+const Home = (props: Props) => {
+  const { handleLogin, handleLogout } = props;
+
+  return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      {props.userId ? (
+        <button
+          onClick={() => {
+            googleLogout();
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <GoogleLogin onSuccess={handleLogin} onError={() => console.log("Error Logging in")} />
+      )}
+      
+      <div className="Home-container">
+        <h1>This is the home page</h1>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default Home;
