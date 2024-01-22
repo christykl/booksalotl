6c7cd6383d61dda8dae2e99e652d0cb9387ec1d3
gsrhaecnige <grac@mit.edu>
2024-01-22 14:12:17 -0500

    books api endpoints skeleton

--- server/api.ts
+++ server/api.ts
@@ -25,6 +25,23 @@ router.post("/initsocket", (req, res) => {
 // | write your API methods below!|
 // |------------------------------|
 
+router.get("/test", (req, res) => {
+  res.send({ message: "the API is working!" });
+});
+
+// GET books
+router.get("/books", (req, res) => {
+  // const user = req.user;
+  res.send({ message: "getting user's books" });
+});
+
+// POST a book
+router.post("/books", (req, res) => {
+  // const user = req.user;
+  // smth like user.getBooks().push(newBook);
+  res.send({ message: "pushing new book into user's books" });
+});
+
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
   const msg = `Api route not found: ${req.method} ${req.url}`;
