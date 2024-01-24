8b21f629f0fbd18d438698807ae1adc0d297fb8a
gsrhaecnige <grac@mit.edu>
2024-01-24 18:12:18 -0500

    basic books styling

--- client/src/components/modules/LibraryCard.tsx
+++ client/src/components/modules/LibraryCard.tsx
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 import "./Modal.css";
 
-
 const LibraryCard = ({ book, userId }) => {
   const [show, setShow] = useState<boolean>(false);
   const [bookItem, setItem] = useState();
 
-  let thumbnail =
-    book.cover;
+  let thumbnail = book.cover;
   if (thumbnail != undefined) {
     return (
       <div>
@@ -22,7 +20,7 @@ const LibraryCard = ({ book, userId }) => {
           <img src={thumbnail} alt="" />
           <div className="bottom">
             <h3 className="title">{book.title}</h3>
-            <h3 className="title">{book.authors}</h3>
+            <h3 className="subtitle">{book.authors}</h3>
           </div>
         </div>
         <Modal userId={userId} show={show} item={bookItem} onClose={() => setShow(false)} />
