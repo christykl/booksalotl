a9e6d45c642ac5901699dd0935320407cf02f3a6
Janelle Cai <jcai18@mit.edu>
2024-01-27 14:57:16 -0500

    getting rid of singlebook component

--- client/src/components/modules/SingleBook.tsx
+++ client/src/components/modules/SingleBook.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { Book } from "../pages/Profile";
-import { Card, Image, Title, Text } from '@mantine/core';
+// import React from "react";
+// import { Book } from "../pages/Profile";
+// import { Card, Image, Title, Text } from '@mantine/core';
 
-import "./SingleBook.css";
+// import "./SingleBook.css";
 
-type SingleBookProps = {
-  userId: string;
-  book: Book;
-};
+// type SingleBookProps = {
+//   userId: string;
+//   book: Book;
+// };
 
-const SingleBook = (props: SingleBookProps) => {
-  const { book } = props;
+// const SingleBook = (props: SingleBookProps) => {
+//   const { book } = props;
 
-  return (
-    <Card className="single-book-card">
-      {book.bookCover && <Image src={book.bookCover} alt={book.title} className="book-cover" />}
-      <div className="book-details">
-        <Title order={3}>{book.title}</Title>
-        <Text>Author: {book.authors}</Text>
-        {book.rating && <Text>Rating: {book.rating}</Text>}
-      </div>
-    </Card>
-  );
-};
+//   return (
+//     <Card className="single-book-card">
+//       {book.bookCover && <Image src={book.bookCover} alt={book.title} className="book-cover" />}
+//       <div className="book-details">
+//         <Title order={3}>{book.title}</Title>
+//         <Text>Author: {book.authors}</Text>
+//         {book.rating && <Text>Rating: {book.rating}</Text>}
+//       </div>
+//     </Card>
+//   );
+// };
 
-export default SingleBook;
+// export default SingleBook;
