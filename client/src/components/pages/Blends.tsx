b16295f0327e88a0221c8f6370d71c82f2508774
Christy Li <ckl@mit.edu>
2024-01-28 02:40:12 -0500

    generate blends page with both users libraries

--- client/src/components/pages/Blends.tsx
+++ client/src/components/pages/Blends.tsx
@@ -3,7 +3,9 @@ import "./Blends.css"
 import { useParams } from 'react-router-dom';
 import { get, post, remove } from "../../utilities";
 import LibraryCard from "../modules/LibraryCard";
-import User from "../../../../shared/User";
+import { User } from "../../../../server/models/User";
+import { Book } from "../../../../server/models/Book";
+import "./Books.css";
 
 type Blend = {
   _id: string;
@@ -17,6 +19,9 @@ type BlendsProps = {
 const Blends = (props: BlendsProps) => {
   const { id } = useParams();
   const [username, setUsername] = useState<string>("Janelle Cai");
+  const [userLibrary, setUserLibrary] = useState<User[]>([]);
+  const [library, setLibrary] = useState<Book[]>([]);
+  const [username2, setUsername2] = useState<string>("Janelle Cai");
 
   useEffect(() => {
     get("/api/whoami")
@@ -27,9 +32,55 @@ const Blends = (props: BlendsProps) => {
       })
   }, []);
 
+  useEffect(() => {
+    get("/api/users").then((users: User[]) => {
+      setUserLibrary(users);
+    });
+  }, []);
+
+  useEffect(() => {
+    get("/api/books").then((books: Book[]) => {
+      setLibrary(books);
+    });
+  }, []);
+
+  const getUsername2 = (id2) => {
+    for (let i = 0; i < userLibrary.length; i++) {
+      if (userLibrary[i]._id === id2) {
+        setUsername2(userLibrary[i].name);
+      }
+    }
+  }
+
   return <div>
-    <h1>{username}</h1>
-    <h1>{id}</h1>
+    <div className="u-textCenter">
+      <h3>{username}'s Library</h3>
+    </div>
+    <div className="library-container">
+      {library.map((book, index) => {
+        // console.log(book);
+        if (book.reader_id && book.reader_id == props.userId)
+          return (
+            <div className="Books-card">
+              <LibraryCard userId={props.userId} book={book} key={book._id} />
+            </div>
+          );
+      })}
+    </div>
+    <div className="u-textCenter">
+      <h3>{username2}'s Library</h3>
+    </div>
+    <div className="library-container">
+      {library.map((book, index) => {
+        // console.log(book);
+        if (book.reader_id && book.reader_id == id)
+          return (
+            <div className="Books-card">
+              <LibraryCard userId={props.userId} book={book} key={book._id} />
+            </div>
+          );
+      })}
+    </div>
   </div>
 };
 
