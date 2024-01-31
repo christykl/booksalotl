7fdf92d634a37e9c40207a18d53fbf978f3ce538
gsrhaecnige <grac@mit.edu>
2024-01-30 23:21:07 -0500

    modal fonts fix

--- client/src/components/modules/BookInfo.tsx
+++ client/src/components/modules/BookInfo.tsx
@@ -3,27 +3,27 @@
 import React, { useEffect, useState } from "react";
 import Genre from "./Genre";
 
-
 const BookInfo = ({ item, onClose, datecb, ratingcb, genrecb, addbook, dropdowncb, currentcb }) => {
   let thumbnail =
     item.volumeInfo && item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
   let today = new Date();
-  const [genre, setGenre] = useState<string>(""); 
+  const [genre, setGenre] = useState<string>("");
   const [date, setDate] = useState<string>(today.toString());
   const [rating, setRating] = useState<number>(0);
-  const [submit, setSubmit] = useState<boolean>(false); 
+  const [submit, setSubmit] = useState<boolean>(false);
   const [current, setCurrent] = useState<boolean>(false);
 
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent default form submission behavior
     setSubmit(true);
-  }
+  };
 
   useEffect(() => {
     if (submit) {
       dropdowncb(); // Close the dropdown
       addbook(item); // Add the book to the library
-    }}, [submit]); 
+    }
+  }, [submit]);
 
   useEffect(() => {
     datecb(new Date(date));
@@ -31,12 +31,12 @@ const BookInfo = ({ item, onClose, datecb, ratingcb, genrecb, addbook, dropdownc
 
   useEffect(() => {
     ratingcb(rating);
-  }, [rating]); 
+  }, [rating]);
 
   useEffect(() => {
     genrecb(genre);
-  }, [genre]);  
-  
+  }, [genre]);
+
   const handleGenreChange = (event) => {
     setGenre(event.target.value);
   };
@@ -49,35 +49,57 @@ const BookInfo = ({ item, onClose, datecb, ratingcb, genrecb, addbook, dropdownc
     <div>
       <div className="overlay">
         <div className="overlay-inner">
-          <button className="close" onClick={onClose}>X</button>
+          <button className="close" onClick={onClose}>
+            X
+          </button>
           <div className="inner-box">
             <img src={thumbnail} alt="" />
             <div className="info">
-            <h1>{item.volumeInfo.title}</h1>
-               <h3>{item.volumeInfo.authors}</h3>
-               <h5>
-                 {item.volumeInfo.publisher}
-                 <span>{item.volumeInfo.published_date}</span>
-               </h5>
-               <br />
+              <h2>{item.volumeInfo.title}</h2>
+              <h3>{item.volumeInfo.authors}</h3>
+              <h5>
+                {item.volumeInfo.publisher}
+                <span>{item.volumeInfo.published_date}</span>
+              </h5>
+              <br />
               {/* ... Other info elements ... */}
               <form onSubmit={handleSubmit}>
                 {!current && (
                   <>
                     <label htmlFor="date">Date Completed:</label>
-                    <input type="date" min="1900-01-01" id="date" name="date" value={date.toString()} onChange={(e) => setDate(e.target.value)}/>
+                    <input
+                      type="date"
+                      min="1900-01-01"
+                      id="date"
+                      name="date"
+                      value={date.toString()}
+                      onChange={(e) => setDate(e.target.value)}
+                    />
                   </>
                 )}
                 <label htmlFor="current">Currently Reading:</label>
-                <input type="checkbox" id="current" name="current" checked={current} onChange={(e) => setCurrent(e.target.checked)}/>
-                <br/>
+                <input
+                  type="checkbox"
+                  id="current"
+                  name="current"
+                  checked={current}
+                  onChange={(e) => setCurrent(e.target.checked)}
+                />
+                <br />
                 <label htmlFor="genre">Genre:</label>
                 <Genre onChange={handleGenreChange} value={genre} />
-                <br/>
+                <br />
                 <label htmlFor="rating">Rating:</label>
-                <input type="number" id="rating" name="rating" placeholder="Rating" value={rating} onChange={(e) => setRating(Number(e.target.value))}/>  
-                <br/>
-                <button type="submit">Add Book</button>
+                <input
+                  type="number"
+                  id="rating"
+                  name="rating"
+                  placeholder="Rating"
+                  value={rating}
+                  onChange={(e) => setRating(Number(e.target.value))}
+                />
+                <br />
+                <button type="submit">Add</button>
               </form>
             </div>
           </div>
@@ -87,7 +109,7 @@ const BookInfo = ({ item, onClose, datecb, ratingcb, genrecb, addbook, dropdownc
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default BookInfo;
